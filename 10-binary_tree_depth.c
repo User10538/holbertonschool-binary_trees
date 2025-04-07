da2b8e11f2293c93c438df8ec246a8b678fe79ff
User10538 <10538@holbertonstudents.com>
2025-04-07 10:51:07 +0000

    Fix error

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,7 +12,7 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	/*The number of edges from the node up to the root of the tree.*/
-	size_t depth;
+	size_t depth = 0;
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
