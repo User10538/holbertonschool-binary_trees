ac3cba33f2a5e80306f48e5144cd523e753a1c02
User10538 <10538@holbertonstudents.com>
2025-04-07 01:31:37 +0000

    function that inserts a node as the left-child of another node

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function that inserts a node as the left-child of another node
+ * @parent: pointer to the node to insert the left-child in
+ * @value:  value to store in the new node
+ *
+ * return: pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	/*Allocate memory for new nodes*/
+        binary_tree_t *new_nodes = malloc(sizeof(binary_tree_t));
+
+        if (new_nodes == NULL) /**check if allocation works*/
+                return (NULL);
+
+	/*initialise the node with given value*/
+        new_nodes->n = value;
+        new_nodes->parent = parent;
+        new_nodes->left = NULL;
+        new_nodes->right = NULL;
+
+	/* If the parent already has a left child, we need to shift the existing left child*/
+
+	if ( parent->left != NULL)
+	{
+		new_nodes->left = parent->left;
+		/*update parent of the existing left child to point to the new nodes*/
+		 parent->left->parent = new_nodes;
+	}
+
+	parent->left = new_nodes;
+	return (new_nodes);
+
+
+}
+
--- binary_trees.h
+++ binary_trees.h
@@ -23,12 +23,21 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 /**
+ * binary_tree_insert_left - function that inserts a node as the left-child of another node
+ * @parent: pointer to the node to insert the left-child in
+ * @value:  value to store in the new node
+ *
+ * return: pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/**
  * binary_tree_node -  function that creates a binary tree node
  *
- * @parent: binary_tree_t
- * @value: int
+ * @parent: a pointer to the parent node of the node to create
+ * @value: value to put in the new node
  *
- * Return: length of printed tree after process
+ * Return: a pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
