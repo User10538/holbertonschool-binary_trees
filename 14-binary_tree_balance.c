fe5578d37ac27ce0d7e29e0170cf73ce3850afe4
User10538 <10538@holbertonstudents.com>
2025-04-08 01:08:21 +0000

    Test

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "binary_trees.h"
+
 /**
  * binary_tree_balance - function that measures
  * the balance factor of a binary tree
@@ -9,38 +9,42 @@
  *
  * Return: 1 if node is a root, otherwise 0
  */
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+    size_t left_height = 0, right_height = 0;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+    if (tree->left)
+        left_height = 1 + binary_tree_height(tree->left);
+    if (tree->right)
+        right_height = 1 + binary_tree_height(tree->right);
 
-	return (left_height - right_height);
+    if (left_height > right_height)
+        return (left_height);
+    else
+        return (right_height);
 }
 
 /**
- * binary_tree_height - function that measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: Pointer to the root node of the tree.
  *
- * Return: height of the tree
+ * Return: The balance factor of the tree.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+    int left_height = 0, right_height = 0;
 
-	if (tree == NULL)
-		return (0);
-	if (tree->left)
-		left_height = 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		right_height = 1 + binary_tree_height(tree->right);
+    if (tree == NULL)
+        return (0);
 
-	if (left_height > right_height)
-		return (left_height);
-	else
-		return (right_height);
+    /* Calculate the heights of left and right subtrees */
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    /* Return the difference between the left and right subtree heights */
+    return (left_height - right_height);
 }
+
