2ecdaeb7107c5dac407749dac4631f58f405af58
User10538 <10538@holbertonstudents.com>
2025-04-11 01:07:57 +0000

    Balance sort

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,11 +9,14 @@
  */
 int tree_height(const binary_tree_t *tree)
 {
+	int left;
+	int right;
+
 	if (tree == NULL)
 		return (0);
 
-	int left = tree_height(tree->left);
-	int right = tree_height(tree->right);
+	left = tree_height(tree->left);
+	right = tree_height(tree->right);
 
 	return ((left > right ? left : right) + 1);
 }
@@ -25,11 +28,14 @@ int tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height;
+	int right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	int left_height = tree->left ? tree_height(tree->left) : 0;
-	int right_height = tree->right ? tree_height(tree->right) : 0;
+	left_height = tree->left ? tree_height(tree->left) : 0;
+	right_height = tree->right ? tree_height(tree->right) : 0;
 
 	return (left_height - right_height);
 }
